e6d738772e4d19d10047d398ea81082c9174cc78
Kalon <kaloncpu57@gmail.com>
2016-04-05 02:23:36 -0600

    Golden Cookie
    Barely working on this whole project

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 var mainCookie = document.querySelector("#bigCookie");
 var goldenCookie = document.querySelector("#goldenCookie");
+var products = [];
+for (var i = 0; i < 14; i++) {
+  products.push(document.querySelector("#product" + i));
+}
 
 var mainClicker = setInterval(function () {
   mainCookie.click();
+  goldenCookie.click();
 }, 1);
